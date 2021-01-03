6ec12c40ddad2193fcb0ceb254a53631b60b6f6a
Hector Herrera <hectorsum81@hotmail.com>
2021-01-02 19:15:45 -0500

    new colors added to theme.js

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,5 +1,6 @@
 export const darkThemeBG = '#2B2B2B';
-export const darkThemeFG = '#F5F5F5';
-export const navbarHoverFG = '#EA6A25';
+export const brandColor = '#FFFF49';
+export const titleColor = '#F5F5F5';
+export const paragraphColor = '#ADAAB1';
 export const buttonColor = '#EF8B50';
 export const buttonColorHover = '#EA6A25';
