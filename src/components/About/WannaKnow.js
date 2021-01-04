08bfe8f5505a55b297c1f3850bea45c947628d56
Hector Herrera <hectorsum81@hotmail.com>
2021-01-04 00:42:51 -0500

    new components (section and button) were inserted

---
+++ src/components/About/WannaKnow.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+import { AboutButton } from '../Button/AboutButton'
+
+const WannaKnowSection = styled.div `
+  position:relative;
+  background-color: ${props => props.theme.darkThemeBG};
+  padding: 5rem;
+  & p{
+    color: ${props => props.theme.paragraphColor};
+    font-size:18px;
+    line-height:26px;
+  }
+`;
+
+const WannaKnowTitle = styled.h1 `
+  margin:0px 0px 10px;
+  font-size:40px;
+  color: ${props => props.theme.titleColor};
+`;
+
+export const WannaKnow = () => {
+  return (
+    <WannaKnowSection>
+      <WannaKnowTitle>Who am I? 🎈</WannaKnowTitle>
+      <p>I have a unique ability to translate big-picture vision into successful design solutions. I question assumptions, turn ambiguity into clarity, and radically empathize with users to craft valuable products and joyful experiences.
+      </p>
+      <AboutButton/>
+    </WannaKnowSection>
+  )
+}
---
+++ src/components/Button/DownloadResume.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Button = styled.button `
+  padding: 15px;
+  width:160px;
+  color:black;
+  background-color: ${props => props.theme.brandColor};
+  border:none;
+  font-size:16px;
+  font-weight:600;
+  margin-top:10px;
+  font-family: 'Space Mono', monospace;
+  outline:none;
+  & a{
+    text-decoration:none;
+    color:black;
+  }
+  
+  &:hover{
+    border:none;
+    cursor: pointer;
+    font-size:16px;
+    background-color:transparent;
+    transition: all ease-out 0.5s;
+    color: ${props => props.theme.titleColor};
+    border: 2px solid ${props => props.theme.titleColor};
+    & a{
+      text-decoration:none;
+      color:white;
+    }
+  }
+`;
+
+
+export const DownloadResume = () => {
+  return (
+    
+      <a href="https://drive.google.com/file/d/1pcP43X2LYSaYfDVQC2U01B7LLw1SjqB4/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+        <Button>My Resume 📌</Button>
+      </a>
+  )
+}
