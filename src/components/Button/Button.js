2f53accd0c10d3ea40d5227e939b6efbd6cffa9a
Hector Herrera <hectorsum81@hotmail.com>
2021-01-12 03:44:28 -0500

    button styles updated

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,23 +1,24 @@
 import styled from "styled-components";
 
-export const Button = styled.button`
-  padding: 10px;
-  width:100%;
-  color:black;
-  background-color: ${props => props.theme.brandColor};
-  border:none;
-  font-size:16px;
+export const Button = styled.a`
+  width : ${props => props.workdetail ? 'auto' : '100px' };
+  color:${props => props.theme.titleColor};;
+  font-size:14px;
   font-weight:600;
-  /* margin-top:10px; */
   font-family: 'Space Mono', monospace;
-  outline:none;
+  text-align:center;
+  text-decoration:none;
+  display:flex;
+  align-items:center;
+  & svg{
+    margin-right:5px;
+  }
   &:hover{
     border:none;
     cursor: pointer;
-    font-size:16px;
+    text-decoration:underline;
     background-color:transparent;
-    transition: all ease-out 0.5s;
-    color: ${props => props.theme.titleColor};
-    border: 2px solid ${props => props.theme.titleColor};
+    transition: all ease-out 0.2s;
+    color: ${props => props.theme.brandColor};
   }
 `;
