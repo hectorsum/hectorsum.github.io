8a4d2f934985d55c01065299b95ee9768c0deb96
Hector Herrera <hectorsum81@hotmail.com>
2021-01-14 22:20:22 -0500

    Related Project component created

--- src/components/Work/ProjectCard.js
+++ src/components/Work/ProjectCard.js
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { WorkButton } from '../Button/WorkButton';
 
-export const ProjectCard = ({id,name,description}) => {
+export const ProjectCard = ({id,name,description,preview_images}) => {
+  console.log(preview_images)
   return (
     <div className="col-lg-4 col-md-6 p-0 px-2">
       <Card key={id} className="card m-0 mb-3">
-        <PreviewImage src={`./project-images/${id}.JPG`} alt={name} className="card-img-top"/>
+        <PreviewImage src={Object.keys(preview_images)[0]} alt={name} className="card-img-top"/>
         <div className="card-body">
           <TitleProject>{name}</TitleProject>
           <DescriptionProject className="card-text">{description}</DescriptionProject>
@@ -26,11 +27,7 @@ const Underline = styled.hr `
 `;
 
 const Card = styled.div `
-  ${'' /* width: 90%; */}
-  ${'' /* height: auto; */}
   background-color:#191919;
-  /* border:2px solid white; */
-  ${'' /* justify-self:center; */}
   margin:8px;
   padding:8px;
   box-shadow: rgba(87, 87, 87, 60) 0px 0.2rem 0.4rem;
