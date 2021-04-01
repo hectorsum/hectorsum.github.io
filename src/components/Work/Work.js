a409031216c8bb563ea2d13314c0e8cb6f65d271
Hector Herrera <hectorsum81@hotmail.com>
2021-04-01 18:45:50 -0500

    Pagination added

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useProjects } from '../../hooks/useProjects';
 import { ProjectCard } from './ProjectCard';
+import projects_json from '../../assets/json/projects.json';
+import ReactPaginate from 'react-paginate';
 
 export const Work = () => {
-  const {data,loading} = useProjects();
-  loading ? console.log('Loading..') : console.log(data);
+  // const {data,loading} = useProjects()
+  const [projects, setProjects] = useState(projects_json.slice(0,projects_json.length));
+  const [pageNumber, setPageNumber] = useState(0);
+  const projectsPerPage = 6;
+  const projectsVisited = pageNumber * projectsPerPage;
+  const displayProjects = projects.slice(projectsVisited, projectsVisited + projectsPerPage).map(project => {
+    return (
+      <ProjectCard key={project.id} {...project} />
+    );
+  })
+  console.log('projects: ',projects)
+  // loading ? console.log('Loading..') : console.log(data);
+  const pageCount = Math.ceil(projects.length / projectsPerPage);
+  const changePage = ({selected}) => {
+    setPageNumber(selected)
+  }
   document.title = 'Work | Hector Herrera';
   return (
     <WorkSection>
       <WorkSectionTitle>work 🚀</WorkSectionTitle>
       <ProjectsWrapper className="col-lg-12 mt-5 mb-5 d-flex justify-content-center">
         <ContainerProject className="row">
-          {
-            data.map( data => (
-              <ProjectCard key={data.id} {...data} />
-            ))
-          }
+          {displayProjects}
+          <ReactPaginate 
+            previousLabel={"Previous"}
+            nextLabel={"Next"}
+            pageCount={pageCount}
+            onPageChange={changePage}
+            containerClassName={"paginationBttns"}
+            previousLinkClassName={"previousBttn"}
+            nextLinkClassName={"nextBttn"}
+            disabledClassName={"paginationDisabled"}
+            activeClassName={"paginationActive"}
+          />
         </ContainerProject>
       </ProjectsWrapper>
     </WorkSection>
